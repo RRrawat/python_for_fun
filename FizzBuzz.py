7d27a19697407943d01b8517cdebf69a3974b267
Rahul Rawat <rahulrawat1097@gmail.com>
2021-04-24 15:27:34 +0530

    Added  description of FizzBuzz problem

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,4 +1,11 @@
-n=int(input("Enter a number"))
+#Writing a short program that prints each number from 1 to n on a new line. 
+
+#For each multiple of 3, print "Fizz" instead of the number. 
+
+#For each multiple of 5, print "Buzz" instead of the number. 
+
+#For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.n=int(input("Enter a number"))
+
 for x in range(n):
     if x%3==0 and x%5==0:
         print("FizzBuzz")

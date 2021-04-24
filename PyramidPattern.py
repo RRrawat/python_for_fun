027c2a95007900e9f387b5f92a20e23af3f30df1
Rahul Rawat <rahulrawat1097@gmail.com>
2021-04-24 15:20:15 +0530

    Added pyramind Pattern in python

---
+++ PyramidPattern.py
@@ -0,0 +1,9 @@
+n=int(input("Enter a number:"))
+for i in range(n):
+  #for printing space 
+    for j in range(n-i-1):
+        print(" ", end=" ")
+   #for printing stars
+    for k in range(2*i+1):
+        print("*", end=" ")
+    print()

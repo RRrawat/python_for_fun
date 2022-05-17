cd65d14702a9ad4f1a42a665a1aea30fa0c12a94
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-05-17 09:36:54 +0530

    Create Sum_of_squares_of_first_n_natural_numbers.py

---
+++ Sum_of_squares_of_first_n_natural_numbers.py
@@ -0,0 +1,44 @@
+#Given a positive integer N. The task is to find 12 + 22 + 32 + ….. + N2.
+
+# Python3 Program to
+# find sum of square
+# of first n natural 
+# numbers
+  
+  
+# Return the sum of
+# square of first n
+# natural numbers
+def squaresum(n) :
+  
+    # Iterate i from 1 
+    # and n finding 
+    # square of i and
+    # add to sum.
+    sm = 0
+    for i in range(1, n+1) :
+        sm = sm + (i * i)
+      
+    return sm
+  
+# Driven Program
+n = 4
+print(squaresum(n))
+  #Output=30
+  
+#Second method
+
+# Python3 Program to
+# find sum of square 
+# of first n natural 
+# numbers
+  
+# Return the sum of 
+# square of first n
+# natural numbers
+def squaresum(n) :
+    return (n * (n + 1) * (2 * n + 1)) // 6
+  
+# Driven Program
+n = 4
+print(squaresum(n))

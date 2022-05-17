a9e9507efc0812a7d90829e4c3e2216c1292eda3
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-05-17 09:39:30 +0530

    Update Sum_of_squares_of_first_n_natural_numbers.py

--- Sum_of_squares_of_first_n_natural_numbers.py
+++ Sum_of_squares_of_first_n_natural_numbers.py
@@ -45,3 +45,17 @@ n = 4
 print(squaresum(n))
 
 #Output=30  
+
+/*****************************************************************************************************************************/
+# Python Program to find sum of square of first
+# n natural numbers. This program avoids
+# overflow upto some extent for large value
+# of n.y
+  
+def squaresum(n):
+    return (n * (n + 1) / 2) * (2 * n + 1) / 3
+  
+# main()
+n = 4
+print(squaresum(n));
+  

f8b4e3de30a59195cca6d14eefc8a26ecfe0d804
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-05-18 09:08:44 +0530

    Update caught_speeding.py

--- caught_speeding.py
+++ caught_speeding.py
@@ -10,3 +10,23 @@ caught_speeding(65, False) → 1
 caught_speeding(65, True) → 0
 
 """
+def caught_speeding(speed, is_birthday):
+    if is_birthday:
+        if speed <= 65:
+            print("0")
+        elif speed >= 66 and speed <= 85:
+            print("1")
+        elif speed >= 86:
+            print("2")
+    else:
+        if speed <= 60:
+            print("0")
+        elif speed >= 61 and speed <= 80:
+            print("1")
+        elif speed >= 81:
+            print("2")
+        
+    
+caught_speeding(60, False) #→ 0
+caught_speeding(65, False) #→ 1
+caught_speeding(65, True) #→ 0

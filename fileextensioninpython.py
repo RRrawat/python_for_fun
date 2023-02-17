176fd96ac17848d55a7b2a692bacac5498c8b8c9
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2023-02-17 12:52:44 +0530

    Update fileextensioninpython.py

--- fileextensioninpython.py
+++ fileextensioninpython.py
@@ -1,1 +1,11 @@
+#Method 1
+#spilt
+a = ['path1/f1/f2/file1.csv', "path1/f1/f2/file2.csv", "path3/f3/f3/file3.csv", "path4/f4/f4/file4.csv"]
+b = [] 
+for path in a:
+    a = path.split('.', )
+    a= a[1]
+    b.append(a)
+print(b)
 
+#method 2

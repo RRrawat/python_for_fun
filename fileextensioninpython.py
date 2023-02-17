ffcc00018d022358e8e00f041b31708a8dd30f17
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2023-02-17 12:58:49 +0530

    Update fileextensioninpython.py

--- fileextensioninpython.py
+++ fileextensioninpython.py
@@ -9,3 +9,12 @@ for path in a:
 print(b)
 
 #method 2
+import os
+path_list = ['path1/f1/f2/file1.csv', "path1/f1/f2/file2.csv", "path3/f3/f3/file3.csv", "path4/f4/f4/file4.csv"]
+ext = []
+for path in path_list:
+    file_name, file_extension = os.path.splitext(path)
+    print(file_name)
+    ext.append(file_extension)
+print(ext)
+    

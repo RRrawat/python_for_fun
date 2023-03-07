12e323ee3fb5e83926ab5887f035ffdafd11da9e
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2023-03-06 16:10:41 -1200

    Update OCRT_ext_from_Image.py

--- OCR Text from Image/OCRT_ext_from_Image.py
+++ OCR Text from Image/OCRT_ext_from_Image.py
@@ -2,14 +2,17 @@
 OCR is a method to recognize text from digital and scanned documents. 
 Many Developers use it to read handwritten data. Below Python Code will 
 help you convert scanned images to OCR text format.
+
 """
 
 # pip install pytesseract
 import pytesseract
 from PIL import Image
- 
+
+#file path
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
- 
+
+
 t=Image.open("img.png")
 text = pytesseract.image_to_string(t, config='')
 print(text)
